e6bf395db61085d85480f36582d8c8a92eafd6d4
shubham sharma <shubhamtech004@gmail.com>
2025-06-23 23:32:31 +0530

    working on this app

---
+++ screens/UpdateTask.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UpdateTask = () => {
+  return (
+    <View>
+      <Text>AddTask</Text>
+      <Text>.......................</Text>
+    </View>
+  )
+}
+
+export default UpdateTask
+
+const styles = StyleSheet.create({})
